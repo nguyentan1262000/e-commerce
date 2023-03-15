92e244c42e3bcdb41dfe0782b4b0f67dfbf528f3
unknown <nguyentan1262000@gmail.com>
2023-03-15 16:59:59 +0700

    update class Config

--- src/main/java/com/demo/ecommerce/status/Config.java
+++ src/main/java/com/demo/ecommerce/status/Config.java
@@ -5,5 +5,12 @@ public class Config {
     public static final int TYPE_USER_CLIENT = 2;
     public static final String NAME_TYPE_USER_1 = "ADMIN";
     public static final String NAME_TYPE_USER_2 = "CLIENT";
-
+    public static final String[] NAME_TYPE_PRODUCT =  {
+            "Thể Thao",
+            "Đồ ăn",
+            "Quần áo nam",
+            "Quần áo nữ",
+            "Giày nam",
+            "Giày nữ"
+    };
 }
